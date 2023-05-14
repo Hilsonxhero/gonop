dfa06a5897b8eea2ec559e1aeee15047ac7afd00
Hilson <hilsonxhero.me@gmail.com>
2023-05-14 19:35:02 +0330

    chore : updates

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 	"myapp/data"
 	"net/http"
+	"time"
 
-	"github.com/CloudyKit/jet/v6"
 	"github.com/hilsonxhero/napoleon"
 )
 
@@ -15,12 +15,8 @@ type Handlers struct {
 }
 
 func (h *Handlers) Home(w http.ResponseWriter, r *http.Request) {
-	myData := "bar"
-	h.App.Session.Put(r.Context(), "foo", myData)
-	myValue := h.App.Session.GetString(r.Context(), "foo")
-	vars := make(jet.VarMap)
-	vars.Set("foo", myValue)
-	err := h.App.Render.Page(w, r, "home", vars, nil)
+	defer h.App.LoadTime(time.Now())
+	err := h.App.Render.Page(w, r, "home", nil, nil)
 	if err != nil {
 		h.App.ErrorLog.Println("error rendering:", err)
 	}
--- routes.go
+++ routes.go
@@ -26,6 +26,12 @@ func (a *application) routes() *chi.Mux {
 
 	a.get("/crypto", a.Handlers.TestCrypto)
 
+	a.get("/cache-test", a.Handlers.ShowCachePage)
+	a.post("/api/save-in-cache", a.Handlers.SaveInCache)
+	a.post("/api/get-from-cache", a.Handlers.GetFromCache)
+	a.post("/api/delete-from-cache", a.Handlers.DeleteFromCache)
+	a.post("/api/empty-cache", a.Handlers.EmptyCache)
+
 	a.App.Routes.Get("/create-user", func(w http.ResponseWriter, r *http.Request) {
 		u := data.User{
 			FirstName: "Trevor",
